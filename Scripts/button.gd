de95161d51e5607fdff12eed78ce7e0e17e50cec
grumpysamurai <142862932+grumpysamurai@users.noreply.github.com>
2024-06-17 17:31:04 -0600

    Added button timeout

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -1,8 +1,18 @@
 extends Node2D
-
+@onready var timer = $Timer
+@onready var animated_sprite_2d = $AnimatedSprite2D
 @export var door := AnimationPlayer.new()
 var isopen = false
 func raisedoor():
 	if isopen == false:
 		door.play("Raise")
 		isopen = true
+		timer.start()
+
+	
+
+func _on_timer_timeout():
+	isopen = true
+	animated_sprite_2d.play("notpressed")
+	door.play("Close")
+	isopen = false
